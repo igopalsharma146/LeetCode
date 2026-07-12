8afc80851095ea954ac2838f632b35e26447a027
Gopal Sharma <160736963+igopalsharma146@users.noreply.github.com>
2026-07-12 10:29:28 +0530

    Time: 18 ms (65.85%) | Memory: 12.3 MB (99.01%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -5,6 +5,26 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        # result=[]
+        # def combinations(index,total,subset):
+        #     if total== target :
+        #         result.append(subset[:])
+        #         return
+        #     if index >= len(candidates) :
+        #         return
+        #     if total > target:
+        #         return
+            
+        #     subset.append(candidates[index])
+        #     combinations(index,total + candidates[index],subset)
+
+        #     subset.pop()
+        #     combinations(index+1,total,subset)
+        
+        # combinations(0,0,[])
+        # return result
+        
+        # Another solution by DFS
         result=[]
         def combinations(index,total,subset):
             if total== target :
@@ -15,12 +35,7 @@ class Solution(object):
             if total > target:
                 return
             
-            subset.append(candidates[index])
-            combinations(index,total + candidates[index],subset)
-
-            subset.pop()
-            combinations(index+1,total,subset)
-        
+            for i in range(index,len(candidates)):
+                combinations(i,total+candidates[i],subset+[candidates[i]])
         combinations(0,0,[])
         return result
-        
