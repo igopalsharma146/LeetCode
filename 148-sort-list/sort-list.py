0e8929cb47bd74b1568497d92b3ff342a65ef13e
Gopal Sharma <sharmagopal7316@gmail.com>
2026-07-14 13:26:26 +0530

    Time: 23 ms (99.78%) | Memory: 50.7 MB (76.09%) - LeetSync

---
+++ 148-sort-list/sort-list.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    # # inPlace solution
+    # def sortList(self, head):
+    #     if not head or not head.next:
+    #         return head
+    #     slow, fast = head, head
+    #     prev = None
+    #     while fast and fast.next:
+    #         prev = slow
+    #         slow = slow.next
+    #         fast = fast.next.next
+    #     prev.next = None
+    #     left = self.sortList(head)
+    #     right = self.sortList(slow)
+    #     return self.merge(left, right)
+
+    # def merge(self, l1, l2):
+    #     dummy = ListNode(0)
+    #     cur = dummy
+    #     while l1 and l2:
+    #         if l1.val < l2.val:
+    #             cur.next = l1
+    #             l1 = l1.next
+    #         else:
+    #             cur.next = l2
+    #             l2 = l2.next
+    #         cur = cur.next
+    #     if l1: cur.next = l1
+    #     if l2: cur.next = l2
+    #     return dummy.next
+
+    # another solution
+    def sortList(self,head):
+
+        if not head or not head.next:
+            return head
+        
+        curr=head
+        curr1=head
+        arr=[]
+        while curr:
+            arr.append(curr.val)
+            curr=curr.next
+        
+        arr.sort()
+
+        # Put sorted values back
+        curr = head
+        i = 0
+        while curr:
+            curr.val = arr[i]
+            curr = curr.next
+            i += 1
+        return head
