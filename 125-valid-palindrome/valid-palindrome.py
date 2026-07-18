1e5f6523272a4e1c3aa2ade8d574f2f5191ce5ed
Gopal Sharma <sharmagopal7316@gmail.com>
2026-07-18 11:35:03 +0530

    Time: 15 ms (72.61%) | Memory: 12.6 MB (77.16%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -5,18 +5,37 @@ class Solution(object):
         :rtype: bool
         """
         
-        n=len(s)
-        left,right=0,n-1
-        while left <= right:
-            if ("a" <= s[left] <= "z") or ("A" <= s[left] <= "Z") or ("0" <= s[left] <= "9"):
-                if ("a" <= s[right] <= "z") or ("A" <= s[right] <= "Z") or ("0" <= s[right] <= "9"):
-                    if s[left].lower()==s[right].lower():
-                        left+=1
-                        right-=1
-                        continue
-                    else:
-                        return False
-                right-=1
-                continue
-            left+=1
+        # n=len(s)
+        # left,right=0,n-1
+        # while left <= right:
+        #     if ("a" <= s[left] <= "z") or ("A" <= s[left] <= "Z") or ("0" <= s[left] <= "9"):
+        #         if ("a" <= s[right] <= "z") or ("A" <= s[right] <= "Z") or ("0" <= s[right] <= "9"):
+        #             if s[left].lower()==s[right].lower():
+        #                 left+=1
+        #                 right-=1
+        #                 continue
+        #             else:
+        #                 return False
+        #         right-=1
+        #         continue
+        #     left+=1
+        # return True
+
+
+        left = 0
+        right = len(s) - 1
+        while left < right:
+
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
         return True
