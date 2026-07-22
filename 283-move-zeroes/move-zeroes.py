032a5d43075de9c4b648021acdf86192f140261d
Gopal Sharma <sharmagopal7316@gmail.com>
2026-07-22 11:31:06 +0530

    Time: 4 ms (79.64%) | Memory: 13.6 MB (19.42%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,20 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        i=0
-        n=len(nums)
-        while i<n:
-            if nums[i] == 0:
-                break
-            i+=1
+        j = 0 # Pointer to place the next non-zero element
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                # Swap current element with the element at index j 
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1 # Move j to the next index for placing non-zero
+
+        # i=0
+        # n=len(nums)
+        # while i<n:
+        #     if nums[i] == 0:
+        #         break
+        #     i+=1
         
-        j=i+1
-        while j<n:
-            if nums[j] == 0:
-                j+=1
-                continue
-            else:
-                nums[j],nums[i]=nums[i],nums[j]
-                i+=1
-            j+=1
+        # j=i+1
+        # while j<n:
+        #     if nums[j] == 0:
+        #         j+=1
+        #         continue
+        #     else:
+        #         nums[j],nums[i]=nums[i],nums[j]
+        #         i+=1
+        #     j+=1
         
