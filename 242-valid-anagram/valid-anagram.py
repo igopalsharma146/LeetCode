0e491e23d84c480d629b0307fc1bf982ab791bac
Gopal Sharma <sharmagopal7316@gmail.com>
2026-07-24 16:50:31 +0530

    Time: 15 ms (86.74%) | Memory: 14.5 MB (8.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,29 +5,29 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        d1={}
-        for ch in s:
-            d1[ch]=d1.get(ch,0)+1
+        # d1={}
+        # for ch in s:
+        #     d1[ch]=d1.get(ch,0)+1
         
-        d2={}
-        for ch in t:
-            d2[ch]=d2.get(ch,0)+1
+        # d2={}
+        # for ch in t:
+        #     d2[ch]=d2.get(ch,0)+1
         
-        if d1 == d2:
-            return True
-        else:
-            return False
-
-        # if len(s) != len(t):
+        # if d1 == d2:
+        #     return True
+        # else:
         #     return False
+
+        if len(s) != len(t):
+            return False
         
-        # freq = [0] * 26
-        # for i in range(len(s)):
-        #     freq[ord(s[i]) - ord('a')] += 1
-        #     freq[ord(t[i]) - ord('a')] -= 1
+        freq = [0] * 26
+        for i in range(len(s)):
+            freq[ord(s[i]) - ord('a')] += 1
+            freq[ord(t[i]) - ord('a')] -= 1
         
-        # for i in range(len(freq)):
-        #     if freq[i] != 0:
-        #         return False
+        for i in range(len(freq)):
+            if freq[i] != 0:
+                return False
         
-        # return True
+        return True
