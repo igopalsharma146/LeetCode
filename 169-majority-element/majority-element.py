3237dcbec43dd9cfec062ec3b28e023dddfa9e02
Gopal Sharma <sharmagopal7316@gmail.com>
2026-07-26 13:45:11 +0530

    Time: 15 ms (27.10%) | Memory: 13.7 MB (39.19%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d1={}
+        for num in nums:
+            d1[num]=d1.get(num,0)+1
+        
+        count=float("-inf")
+        maxi=None
+        for value in d1:
+            if d1[value]>count:
+                count=d1[value]
+                maxi=value
+        return maxi
