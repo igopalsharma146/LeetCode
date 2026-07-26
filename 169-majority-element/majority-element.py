33dcbc5b308f636668c12786204c17393478e176
Gopal Sharma <sharmagopal7316@gmail.com>
2026-07-26 13:46:13 +0530

    Time: 2 ms (88.66%) | Memory: 13.5 MB (97.46%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,14 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        d1={}
-        for num in nums:
-            d1[num]=d1.get(num,0)+1
+        # d1={}
+        # for num in nums:
+        #     d1[num]=d1.get(num,0)+1
         
-        count=float("-inf")
-        maxi=None
-        for value in d1:
-            if d1[value]>count:
-                count=d1[value]
-                maxi=value
-        return maxi
+        # count=float("-inf")
+        # maxi=None
+        # for value in d1:
+        #     if d1[value]>count:
+        #         count=d1[value]
+        #         maxi=value
+        # return maxi
+
+        nums.sort()
+        n=len(nums)
+        return nums[n//2]
