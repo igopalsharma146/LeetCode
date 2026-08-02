3c4818ca32f5c6699a9679678008885395780bba
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-02 10:25:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (47.77%) - LeetSync

---
+++ 909-stone-game/stone-game.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        # n = len(piles)
+        # dp = [[0] * n for _ in range(n)]
+
+        # for i in range(n):
+        #     dp[i][i] = piles[i]
+
+        # for length in range(2, n + 1):
+        #     for i in range(n - length + 1):
+        #         j = i + length - 1
+
+        #         takeLeft = piles[i] - dp[i + 1][j]
+        #         takeRight = piles[j] - dp[i][j - 1]
+
+        #         dp[i][j] = max(takeLeft, takeRight)
+
+        # return dp[0][n - 1] > 0
+
+        return True
