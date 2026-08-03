eee275b4d73032642fd277e504eaef3629ea9d93
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-03 09:30:13 +0530

    Time: 924 ms (87.22%) | Memory: 16 MB (35.34%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def stoneGameIII(self, stoneValue):
+        """
+        :type stoneValue: List[int]
+        :rtype: str
+        """
+        n = len(stoneValue)
+        dp = [-10**18] * (n + 1)
+        dp[n] = 0
+
+        for i in range(n - 1, -1, -1):
+            take = 0
+            for k in range(3):
+                if i + k >= n:
+                    break
+                take += stoneValue[i + k]
+                dp[i] = max(dp[i],take - dp[i + k + 1])
+
+        if dp[0] > 0:
+            return "Alice"
+            
+        if dp[0] < 0:
+            return "Bob"
+
+        return "Tie"
