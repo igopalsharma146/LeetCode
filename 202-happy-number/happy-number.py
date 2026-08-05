86d22534532b4308bc3f2376eea3d216c17c3dd8
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-05 20:46:43 +0530

    Time: 3 ms (48.88%) | Memory: 12.2 MB (89.23%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        # if(n==1 or n==7):
+        #     return True
+        # elif(n<10):
+        #     return False
+        # else:
+        #     sum =0
+        #     while(n>0):
+        #         temp = n%10
+        #         sum += temp*temp
+        #         n= n//10
+        #     return self.isHappy(sum) 
+
+        hset = set()
+        while n != 1:
+            if n in hset: return False
+            hset.add(n)
+            n = sum([int(i) ** 2 for i in str(n)])
+        else:
+            return True
