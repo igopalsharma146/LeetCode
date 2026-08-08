b529ad4c8952313de743b8f1293ae321178c4c9c
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-08 11:18:25 +0530

    Time: 5 ms (3.57%) | Memory: 12.4 MB (17.44%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -4,14 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        binary=0
-        while(n>0):
-            bit=n%2
-            binary=binary*10+bit
-            n=n//2
-        binarystring=str(binary)
-        count=0
-        for i in binarystring:
-            if(int(i)==1):
-                count+=1
-        return count
+        def int_to_bin(n):
+            binary=0
+            i=0
+            while(n>0):
+                bit=n%2
+                binary=binary+bit*10**i
+                n=n//2
+                i+=1
+            return binary
+
+        num=int_to_bin(n)
+        res=str(num)
+        return res.count('1')
