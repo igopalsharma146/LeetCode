57cc2e1beea40c05a84a50852c74184e0db79732
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-10 14:39:36 +0530

    Time: 3 ms (97.64%) | Memory: 13.2 MB (19.52%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -4,13 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left,right=0,0
-        jump=0
-        while right<len(nums)-1:
-            farthest=0
-            for i in range(left,right+1):
-                farthest=max(farthest,i+nums[i])
-            left=right+1
-            right=farthest
-            jump+=1
-        return jump
+        # left,right=0,0
+        # jump=0
+        # while right<len(nums)-1:
+        #     farthest=0
+        #     for i in range(left,right+1):
+        #         farthest=max(farthest,i+nums[i])
+        #     left=right+1
+        #     right=farthest
+        #     jump+=1
+        # return jump
+
+        jumps = 0
+        current_end = 0
+        farthest = 0
+        for i in range(len(nums) - 1):
+            # Update the farthest index we can reach
+            if i + nums[i] > farthest:
+                farthest = i + nums[i]
+
+            # If we reach the end of the current jump
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+
+        return jumps
