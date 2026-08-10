eeb7002e5b0807d32dbea3dce412d7e75fba30e1
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-10 14:21:30 +0530

    Time: 41 ms (38.37%) | Memory: 13.4 MB (29.55%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        max_index=0
+        for i in range(0,len(nums)):
+            if i>max_index:
+                return False
+            max_index=max(max_index,i+nums[i])
+        return True
