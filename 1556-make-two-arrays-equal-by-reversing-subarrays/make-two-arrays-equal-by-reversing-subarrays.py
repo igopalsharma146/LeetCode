0e9f32b282b12b52810750f64e1e023f7f792d14
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-13 12:45:39 +0530

    Time: 7 ms (37.97%) | Memory: 12.6 MB (57.22%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
@@ -6,39 +6,38 @@ class Solution(object):
         :rtype: bool
         """
         # ye code run ho jayega per ye sahi tarika nahi h
-
-        # arr.sort()
-        # target.sort()
-        # if target==arr:
-        #     return True
-        # return False
-
-
-        # sahi tarika karne ka
-        i=0
-        while i<len(target):
-            # agar value sahi position me h to reverse karne ki need nahi h
-            if target[i]==arr[i]:
-                i+=1
-                continue
-
-            # finding the position of reversing array
-            left=right=i
-            while right<len(arr) and target[i]!=arr[right]:
-                right+=1
-            
-            # agar target ka koi bhi ek element hame arr na mila to turant false return kar denge
-            if right==len(arr):
-                return False
-            
-            #Reversing the array
-            while left<right:
-                arr[left],arr[right]=arr[right],arr[left]
-                left+=1
-                right-=1
-            i+=1
-        
-        # checking both array
+        arr.sort()
+        target.sort()
         if target==arr:
             return True
         return False
+
+
+        # # sahi tarika karne ka
+        # i=0
+        # while i<len(target):
+        #     # agar value sahi position me h to reverse karne ki need nahi h
+        #     if target[i]==arr[i]:
+        #         i+=1
+        #         continue
+
+        #     # finding the position of reversing array
+        #     left=right=i
+        #     while right<len(arr) and target[i]!=arr[right]:
+        #         right+=1
+            
+        #     # agar target ka koi bhi ek element hame arr na mila to turant false return kar denge
+        #     if right==len(arr):
+        #         return False
+            
+        #     #Reversing the array
+        #     while left<right:
+        #         arr[left],arr[right]=arr[right],arr[left]
+        #         left+=1
+        #         right-=1
+        #     i+=1
+        
+        # # checking both array
+        # if target==arr:
+        #     return True
+        # return False
