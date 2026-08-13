031d54134d2c9ac2da1ff0bf53105cc2a25d0ffa
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-13 11:00:31 +0530

    Time: 7 ms (98.98%) | Memory: 25.9 MB (68.17%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,9 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        freq={}
-        for ch in nums:
-            freq[ch]=freq.get(ch,0)+1
-            if freq[ch]>=2:
-                return True
-        return False
+        # freq={}
+        # for ch in nums:
+        #     freq[ch]=freq.get(ch,0)+1
+        #     if freq[ch]>=2:
+        #         return True
+        # return False
+
+        set_nums = set(nums)
+        if len(nums) == len(set_nums):
+            return False
+        return True        
