da4498849c122c1e4c31781a5dff5bf1d0c2a3d8
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-21 11:51:15 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (27.66%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -5,24 +5,37 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        res=[]
-        curr=head
-        while curr:
-            res.append(curr.val)
-            curr=curr.next
+        # # Brute force solution
+        # res=[]
+        # curr=head
+        # while curr:
+        #     res.append(curr.val)
+        #     curr=curr.next
 
-        # reversing the array
-        def reverse(left,right):
-            while left<=right:
-                res[left],res[right]=res[right],res[left]
-                left+=1
-                right-=1
+        # # reversing the array
+        # def reverse(left,right):
+        #     while left<=right:
+        #         res[left],res[right]=res[right],res[left]
+        #         left+=1
+        #         right-=1
 
-        reverse(0,len(res)-1)
+        # reverse(0,len(res)-1)
 
-        curr1,i=head,0
-        while curr1:
-            curr1.val=res[i]
-            curr1=curr1.next
-            i+=1
-        return head
+        # curr1,i=head,0
+        # while curr1:
+        #     curr1.val=res[i]
+        #     curr1=curr1.next
+        #     i+=1
+        # return head
+
+        # optimal solution
+        if head is None or head.next is None:
+            return head
+        d=None
+        temp=head
+        while temp:
+            x=temp.next
+            temp.next=d
+            d=temp
+            temp=x
+        return d     
