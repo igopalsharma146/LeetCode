4bf6516152c8b9fee1d6ba95989c745d1168f217
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-21 11:43:03 +0530

    Time: 45 ms (93.03%) | Memory: 54.3 MB (31.29%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
@@ -5,21 +5,38 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        res=[]
-        curr=head
+        # Brute force solution
+        # res=[]
+        # curr=head
+        # while curr:
+        #     res.append(curr.val)
+        #     curr=curr.next
+
+        # # reversing the array
+        # def reverse(left,right):
+        #     res[left],res[right]=res[right],res[left]
+
+        # reverse(k-1,len(res)-k)
+
+        # curr1,i=head,0
+        # while curr1:
+        #     curr1.val=res[i]
+        #     curr1=curr1.next
+        #     i+=1
+        # return head
+
+        # optimal solution
+        curr = head
+        for _ in range(k - 1):
+            curr = curr.next
+        
+        node1 = curr
+        curr = curr.next
+        node2 = head
         while curr:
-            res.append(curr.val)
-            curr=curr.next
-
-        # reversing the array
-        def reverse(left,right):
-            res[left],res[right]=res[right],res[left]
-
-        reverse(k-1,len(res)-k)
-
-        curr1,i=head,0
-        while curr1:
-            curr1.val=res[i]
-            curr1=curr1.next
-            i+=1
+            node2 = node2.next
+            curr = curr.next
+        
+        node1.val, node2.val = node2.val, node1.val
+        
         return head
