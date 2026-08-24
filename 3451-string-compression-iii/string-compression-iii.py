b0f764508d6ecdd248039364638d3ed3f73044d7
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-24 13:56:13 +0530

    Time: 115 ms (96.53%) | Memory: 30 MB (14.40%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.py
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -1,21 +1,33 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        read,res=0,""
-        while read < len(word):
-            char = word[read]
-            count = 0
+        res = []
+        cur = word[0]
+        count = 0
+        for ch in word + '!':
+            if ch != cur or count >= 9:
+                res.append(str(count))
+                res.append(cur)
+                count = 0
+                cur = ch
+            count += 1
+        return ''.join(res)
 
-            # Count consecutive same characters
-            while read < len(word) and word[read] == char:
-                read += 1
-                count += 1
+        # read,res=0,""
+        # while read < len(word):
+        #     char = word[read]
+        #     count = 0
 
-            # writing count
-            while count>9:
-                res+=str(9)
-                res+=char
-                count-=9
-            if count>0:
-                res+=str(count)
-                res+=char
-        return res
+        #     # Count consecutive same characters
+        #     while read < len(word) and word[read] == char:
+        #         read += 1
+        #         count += 1
+
+        #     # writing count
+        #     while count>9:
+        #         res+=str(9)
+        #         res+=char
+        #         count-=9
+        #     if count>0:
+        #         res+=str(count)
+        #         res+=char
+        # return res
