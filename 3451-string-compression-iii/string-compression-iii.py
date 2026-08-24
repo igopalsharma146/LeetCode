475b22a47c0861ba41a66db75bc48c164e6d87fc
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-24 13:50:27 +0530

    Time: 235 ms (32.53%) | Memory: 21.9 MB (76.27%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def compressedString(self, word: str) -> str:
+        read,res=0,""
+        while read < len(word):
+            char = word[read]
+            count = 0
+
+            # Count consecutive same characters
+            while read < len(word) and word[read] == char:
+                read += 1
+                count += 1
+
+            # writing count
+            while count>9:
+                res+=str(9)
+                res+=char
+                count-=9
+            if count>0:
+                res+=str(count)
+                res+=char
+        return res
