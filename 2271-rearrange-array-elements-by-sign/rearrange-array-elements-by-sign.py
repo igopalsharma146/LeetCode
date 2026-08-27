cf188475a77f799f9902eacd7a21facabd538cbe
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-27 13:51:06 +0530

    Time: 49 ms (84.97%) | Memory: 42.3 MB (75.99%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -4,17 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        pos=[]
-        neg=[]
-        for ch in nums:
-            if ch<0:
-                neg.append(ch)
+        res=[0]*len(nums)
+        pos=0
+        neg=1
+        for num in nums:
+            if num>0:
+                res[pos]=num
+                pos+=2
             else:
-                pos.append(ch)
-        
-        res=[]
-        i=0
-        while i<len(pos):
-            res.extend([pos[i],neg[i]])
-            i+=1
+                res[neg]=num
+                neg+=2
         return res
+
+
+        # pos=[]
+        # neg=[]
+        # for ch in nums:
+        #     if ch<0:
+        #         neg.append(ch)
+        #     else:
+        #         pos.append(ch)
+        
+        # res=[]
+        # i=0
+        # while i<len(pos):
+        #     res.extend([pos[i],neg[i]])
+        #     i+=1
+        # return res
