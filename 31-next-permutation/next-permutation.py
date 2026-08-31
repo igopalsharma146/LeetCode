5fae6cbb3ec1c00d9f2f3289231f7f7dcd83009d
Gopal Sharma <sharmagopal7316@gmail.com>
2026-08-31 16:09:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (92.80%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+
+        # 1. Find first decreasing element
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        # 2. Find just greater element and swap
+        if i >= 0:
+            j = n - 1
+
+            while nums[j] <= nums[i]:
+                j -= 1
+
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # 3. Reverse suffix
+        left = i + 1
+        right = n - 1
+
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
