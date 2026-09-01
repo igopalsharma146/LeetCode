f07cb4dd0457188ce55c77771ca3627500b4122c
Gopal Sharma <sharmagopal7316@gmail.com>
2026-09-01 11:53:50 +0530

    Time: 43 ms (33.48%) | Memory: 14.8 MB (48.10%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        result = 0
+        prefix_sum = 0
+        d = {0: 1}
+
+        for num in nums:
+            prefix_sum = prefix_sum + num
+
+            if prefix_sum - k in d:
+                result = result + d[prefix_sum - k]
+
+            d[prefix_sum] = d.get(prefix_sum, 0) + 1
+
+        return result
