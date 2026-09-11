260b7f3db1bdb3e922c5e3c64b3f9fd2321e3254
Gopal Sharma <sharmagopal7316@gmail.com>
2026-09-11 14:59:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (96.57%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,14 +1,27 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        if len(nums)==1:
+        if len(nums) == 1:
             return 0
-        peak=None
-        for i in range(0,len(nums)):
-            if i==0 and i < len(nums)-1 and nums[i] > nums[i+1]:
-                peak=i
-            if i>0 and i < len(nums)-1:
-                if nums[i-1]< nums[i] > nums[i+1]:
+        for i,num in enumerate(nums):
+            if i == 0:
+                if nums[i] > nums[i+1]:
                     return i
-            if i>0 and i==len(nums)-1 and nums[i-1]<nums[i]:
-                peak=i
-        return peak
+            elif i == len(nums) - 1:
+                if nums[i-1] < nums[i]:
+                    return i
+            else:
+                if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
+                    return i
+                    
+        # if len(nums)==1:
+        #     return 0
+        # peak=None
+        # for i in range(0,len(nums)):
+        #     if i==0 and i < len(nums)-1 and nums[i] > nums[i+1]:
+        #         peak=i
+        #     if i>0 and i < len(nums)-1:
+        #         if nums[i-1]< nums[i] > nums[i+1]:
+        #             return i
+        #     if i>0 and i==len(nums)-1 and nums[i-1]<nums[i]:
+        #         peak=i
+        # return peak
